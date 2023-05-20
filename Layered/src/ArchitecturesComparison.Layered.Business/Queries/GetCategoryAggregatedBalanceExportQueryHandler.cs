617b62d5afdc393aeda31346b1ab834e13231151
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2023-05-20 21:28:22 +0200

    #10 Add export (PR: #13)

---
+++ Layered/src/ArchitecturesComparison.Layered.Business/Queries/GetCategoryAggregatedBalanceExportQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ArchitecturesComparison.Layered.Business.Common;
+using ArchitecturesComparison.Requests.DTOs;
+using ArchitecturesComparison.Requests.Queries;
+using MediatR;
+
+namespace ArchitecturesComparison.Layered.Business.Queries
+{
+    public class GetCategoryAggregatedBalanceExportQueryHandler : IRequestHandler<GetCategoryAggregatedBalanceExportQuery, ExportResultDto>
+    {
+        private readonly IMediator _mediator;
+        private readonly IDataExporter _dataExporter;
+
+        public GetCategoryAggregatedBalanceExportQueryHandler(IMediator mediator, IDataExporter dataExporter)
+        {
+            _mediator = mediator;
+            _dataExporter = dataExporter;
+        }
+        
+        public async Task<ExportResultDto> Handle(GetCategoryAggregatedBalanceExportQuery request, CancellationToken cancellationToken)
+        {
+            var aggregatedCategoryBalance = await _mediator.Send(new GetCategoryAggregatedBalanceQuery
+            {
+                BudgetId = request.BudgetId,
+                CategoryId = request.CategoryId
+            }, cancellationToken);
+
+            var result = _dataExporter.ExportCategoriesAggregatedData(aggregatedCategoryBalance);
+
+            return result;
+        }
+    }
+}
