3d70f0e4c51241a06eaab084eb4958e567940731
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-05-20 13:24:39 +0200

    #10 Add export queries to Requests library

---
+++ Shared/src/ArchitecturesComparison.Requests/DTOs/ExportResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ArchitecturesComparison.Requests.DTOs
+{
+    public class ExportResultDto
+    {
+        public string FilePath { get; }
+        public string Extension { get; }
+
+        public ExportResultDto(string filePath, string extension)
+        {
+            FilePath = filePath;
+            Extension = extension;
+        }
+    }
+}
