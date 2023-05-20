9a8f47fbb689f605c2a09a01babe0b4f6ceeb73c
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2023-05-20 14:28:36 +0200

    #10 Fix queries return types (PR: #12)

--- Shared/src/ArchitecturesComparison.Requests/Queries/GetCategoryAggregatedBalanceExportQuery.cs
+++ Shared/src/ArchitecturesComparison.Requests/Queries/GetCategoryAggregatedBalanceExportQuery.cs
@@ -1,4 +1,12 @@
-﻿namespace ArchitecturesComparison.Requests.Queries
+﻿using System;
+using ArchitecturesComparison.Requests.DTOs;
+using MediatR;
+
+namespace ArchitecturesComparison.Requests.Queries
 {
-    public class GetCategoryAggregatedBalanceExportQuery : GetCategoryAggregatedBalanceQuery { }
+    public class GetCategoryAggregatedBalanceExportQuery : IRequest<ExportResultDto>
+    {
+        public Guid BudgetId { get; set; }
+        public Guid CategoryId { get; set; }
+    }
 }
