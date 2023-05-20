a9e6474113b903961e8230fb2c4073c8fe85d1d5
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-05-20 13:58:00 +0200

    #10 Fix queries return types

--- Shared/src/ArchitecturesComparison.Requests/Queries/GetCategoriesEntriesExportQuery.cs
+++ Shared/src/ArchitecturesComparison.Requests/Queries/GetCategoriesEntriesExportQuery.cs
@@ -1,4 +1,13 @@
-﻿namespace ArchitecturesComparison.Requests.Queries
+﻿using System;
+using System.Collections.Generic;
+using ArchitecturesComparison.Requests.DTOs;
+using MediatR;
+
+namespace ArchitecturesComparison.Requests.Queries
 {
-    public class GetCategoriesEntriesExportQuery : GetCategoriesEntriesQuery {}
+    public class GetCategoriesEntriesExportQuery : IRequest<ExportResultDto>
+    {
+        public Guid BudgetId { get; set; }
+        public IEnumerable<Guid> CategoriesIds { get; set; }
+    }
 }
