45b0340a2149ce39907722e061b5453f25abe443
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2023-05-21 10:11:58 +0200

    #14 Add Onion Architecture (PR: #15)

--- Onion/src/ArchitecturesComparison.Onion.Application/Facade.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ArchitecturesComparison.Onion.Application
-{
-    public class Facade
-    {
-        public static string Namespace => typeof(Facade).Namespace;
-    }
-}
---
+++ Onion/src/ArchitecturesComparison.Onion.Application/Queries/GetCategoriesEntriesQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ArchitecturesComparison.Onion.Application.Common;
+using ArchitecturesComparison.Requests.DTOs;
+using ArchitecturesComparison.Requests.Queries;
+using MediatR;
+
+namespace ArchitecturesComparison.Onion.Application.Queries
+{
+    public class GetCategoriesEntriesQueryHandler : IRequestHandler<GetCategoriesEntriesQuery, IEnumerable<EntryDto>>
+    {
+        private readonly IBudgetRepository _budgetRepository;
+
+        public GetCategoriesEntriesQueryHandler(IBudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+        
+        public async Task<IEnumerable<EntryDto>> Handle(GetCategoriesEntriesQuery request, CancellationToken cancellationToken)
+        {
+            var budget = await _budgetRepository.GetById(request.BudgetId);
+
+            if (budget is null)
+            {
+                return null!;
+            }
+
+            var materializedCategoriesIds = request.CategoriesIds.ToHashSet();
+            return budget.Categories
+                .Where(x => materializedCategoriesIds.Contains(x.Id))
+                .SelectMany(x => x.Entries)
+                .Select(x =>
+                    new EntryDto(
+                        x.Id,
+                        x.Name.Value,
+                        x.Description.Value,
+                        x.Date.Value,
+                        new TransactionDto(
+                            x.Transaction.Amount,
+                            x.Transaction.BalanceChange)
+                    )
+                );
+        }
+    }
+}
